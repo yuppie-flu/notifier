4bfaf6b3a5224d6b6f94da6ed9060cf6914d5d7b
Kirill Kozlov <yuppie-flu>
2020-08-16 21:00:38 +0200

    Basic SendGrid integration

--- src/main/kotlin/com/github/yuppieflu/notifier/service/Notifier.kt
+++ src/main/kotlin/com/github/yuppieflu/notifier/service/Notifier.kt
@@ -1,7 +1,17 @@
 package com.github.yuppieflu.notifier.service
 
 import com.github.yuppieflu.notifier.domain.NotificationPackage
+import com.sendgrid.Method
+import com.sendgrid.Request
+import com.sendgrid.SendGrid
+import com.sendgrid.helpers.mail.Mail
+import com.sendgrid.helpers.mail.objects.Email
+import com.sendgrid.helpers.mail.objects.MailSettings
+import com.sendgrid.helpers.mail.objects.Personalization
+import com.sendgrid.helpers.mail.objects.Setting
 import org.slf4j.LoggerFactory
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
 import org.springframework.stereotype.Service
 
 interface Notifier {
@@ -9,20 +19,55 @@ interface Notifier {
 }
 
 @Service
-class ConsoleNotifier : Notifier {
-    private val log = LoggerFactory.getLogger(ConsoleNotifier::class.java)
+class SendgridNotifier(
+    private val sendgridProperties: SendgridProperties
+) : Notifier {
+
+    private val log = LoggerFactory.getLogger(SendgridNotifier::class.java)
+
+    private val sendGrid = SendGrid(sendgridProperties.apiKey)
 
     override fun notify(aPackage: NotificationPackage) {
-        log.info("Sending notification:")
-        log.info("To: ${aPackage.email}")
-        log.info("Username: ${aPackage.username}")
-        log.info("Content:")
-        aPackage.subreddits.forEach {
-            log.info("Subreddit: [${it.name}] ${it.topUrl}")
-            it.topPosts.forEach { post ->
-                log.info("Post: ${post.title} [${post.score}]")
-                log.info("URL: ${post.url}")
+        val personalization = Personalization().apply {
+            addDynamicTemplateData("username", aPackage.username)
+            addTo(Email(aPackage.email))
+        }
+
+        val mail = Mail().apply {
+            setFrom(Email(sendgridProperties.fromEmail))
+            setTemplateId(sendgridProperties.templateId)
+            addPersonalization(personalization)
+            if (sendgridProperties.sandboxMode) {
+                setMailSettings(MailSettings())
+                val sandBoxMode = Setting()
+                sandBoxMode.enable = true
+                mailSettings.setSandboxMode(sandBoxMode)
             }
         }
+
+        runCatching {
+            val request = Request().apply {
+                method = Method.POST
+                endpoint = "mail/send"
+                body = mail.build()
+            }
+            sendGrid.api(request)
+        }.fold(
+            onFailure = {
+                log.error("Error sending email via Sendgrid", it)
+            },
+            onSuccess = {
+                log.info("Sendgrid response: status=${it.statusCode}, body=${it.body}")
+            }
+        )
     }
 }
+
+@ConstructorBinding
+@ConfigurationProperties(prefix = "notifier.sendgrid")
+data class SendgridProperties(
+    val apiKey: String,
+    val fromEmail: String,
+    val templateId: String,
+    val sandboxMode: Boolean
+)
