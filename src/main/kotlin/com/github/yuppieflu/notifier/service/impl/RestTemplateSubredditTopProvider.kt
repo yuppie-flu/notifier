bb374f13999d123a03a3550465327b25ff7ac108
Kirill Kozlov <yuppie-flu@users.noreply.github.com>
2020-08-17 12:09:43 +0200

    Reorganise services package

---
+++ src/main/kotlin/com/github/yuppieflu/notifier/service/impl/RestTemplateSubredditTopProvider.kt
@@ -0,0 +1,77 @@
+package com.github.yuppieflu.notifier.service.impl
+
+import com.github.yuppieflu.notifier.domain.PostMeta
+import com.github.yuppieflu.notifier.domain.SubredditTop
+import com.github.yuppieflu.notifier.service.SubredditTopProvider
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
+import org.springframework.stereotype.Service
+import org.springframework.web.client.RestTemplate
+import java.lang.IllegalStateException
+import java.net.URL
+
+@Service
+class RestTemplateSubredditTopProvider(
+    private val restTemplate: RestTemplate,
+    @Value("\${notifier.posts-limit:3}") private val postsLimit: Int,
+    @Value("\${notifier.reddit.url:}") private val redditUrl: String
+) : SubredditTopProvider {
+
+    override fun getTopPosts(subreddit: String): SubredditTop {
+        val httpEntity = HttpEntity<String>(headers)
+
+        val response = restTemplate.exchange(
+            subredditTopQueryUrl(subreddit),
+            HttpMethod.GET,
+            httpEntity,
+            RedditPostListingResponse::class.java
+        )
+        return response.body?.data?.children?.map {
+            PostMeta(
+                subreddit = subreddit,
+                title = it.data.title,
+                score = it.data.score,
+                url = fullPostUrl(it.data.permalink)
+            )
+        }?.let {
+            SubredditTop(
+                name = subreddit,
+                topPosts = it
+            )
+        } ?: throw IllegalStateException("Something went wrong when querying Reddit!")
+    }
+
+    private fun fullPostUrl(postfix: String): URL =
+        URL("$redditUrl$postfix")
+
+    private fun subredditTopQueryUrl(subreddit: String) =
+        "$redditUrl/r/$subreddit/top/.json?limit=$postsLimit&t=day"
+
+    companion object {
+        private val headers = HttpHeaders().also {
+            it.set(HttpHeaders.USER_AGENT, "kotlin-reddit-notifier")
+        }
+    }
+}
+
+private data class RedditPostListingResponse(
+    val data: Data
+)
+
+private data class Data(
+    val children: List<Child>
+)
+
+private data class Child(
+    val kind: String,
+    val data: ChildData
+)
+
+private data class ChildData(
+    val id: String,
+    val title: String,
+    val score: Int,
+    val permalink: String
+)
---
+++ src/main/kotlin/com/github/yuppieflu/notifier/service/impl/SendgridNotifier.kt
@@ -0,0 +1,70 @@
+package com.github.yuppieflu.notifier.service.impl
+
+import com.github.yuppieflu.notifier.domain.NotificationPackage
+import com.github.yuppieflu.notifier.service.Notifier
+import com.sendgrid.Method
+import com.sendgrid.Request
+import com.sendgrid.SendGrid
+import com.sendgrid.helpers.mail.Mail
+import com.sendgrid.helpers.mail.objects.Email
+import com.sendgrid.helpers.mail.objects.MailSettings
+import com.sendgrid.helpers.mail.objects.Personalization
+import com.sendgrid.helpers.mail.objects.Setting
+import org.slf4j.LoggerFactory
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
+import org.springframework.stereotype.Service
+
+@Service
+class SendgridNotifier(
+    private val sendgridProperties: SendgridProperties
+) : Notifier {
+
+    private val log = LoggerFactory.getLogger(SendgridNotifier::class.java)
+
+    private val sendGrid = SendGrid(sendgridProperties.apiKey)
+
+    override fun notify(aPackage: NotificationPackage) {
+        val personalization = Personalization().apply {
+            addDynamicTemplateData("username", aPackage.username)
+            addTo(Email(aPackage.email))
+        }
+
+        val mail = Mail().apply {
+            setFrom(Email(sendgridProperties.fromEmail))
+            setTemplateId(sendgridProperties.templateId)
+            addPersonalization(personalization)
+            if (sendgridProperties.sandboxMode) {
+                setMailSettings(MailSettings())
+                val sandBoxMode = Setting()
+                sandBoxMode.enable = true
+                mailSettings.setSandboxMode(sandBoxMode)
+            }
+        }
+
+        runCatching {
+            val request = Request().apply {
+                method = Method.POST
+                endpoint = "mail/send"
+                body = mail.build()
+            }
+            sendGrid.api(request)
+        }.fold(
+            onFailure = {
+                log.error("Error sending email via Sendgrid", it)
+            },
+            onSuccess = {
+                log.info("Sendgrid response: status=${it.statusCode}, body=${it.body}")
+            }
+        )
+    }
+}
+
+@ConstructorBinding
+@ConfigurationProperties(prefix = "notifier.sendgrid")
+data class SendgridProperties(
+    val apiKey: String,
+    val fromEmail: String,
+    val templateId: String,
+    val sandboxMode: Boolean
+)
